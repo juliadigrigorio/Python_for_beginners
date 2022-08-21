ceb72ba424a3c1681b790863477c5326fb118f78
juliadigrigorio <juliadigrigorio@hotmail.com>
2022-08-21 14:18:33 +0300

    learning basic concepts

---
+++ Basic_concepts.py
@@ -0,0 +1,20 @@
+print(65000*1.15)
+
+#-------------
+
+print(60)
+
+#-------------
+
+print("Coding is an art")
+
+#-------------
+
+print("Shopping List:")
+print("Bread")
+print("Eggs")
+
+#-------------
+
+
+
