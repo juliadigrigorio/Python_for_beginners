ee5c07fd8fe9bba23a74dece6cda5270a7583dbc
juliadigrigorio <juliadigrigorio@hotmail.com>
2022-08-21 14:22:57 +0300

    strings

---
+++ strings.py
@@ -0,0 +1,22 @@
+print("Python is fun!")
+
+print('Always look on the bright side of life')
+
+#-----------
+
+print("42")
+
+#-----------
+
+print('Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!')
+
+#-----------
+
+print("He said: \"cool!\".")
+
+#-----------
+
+print('I\'m learning Python. It\'s easy.')
+
+#-----------
+
