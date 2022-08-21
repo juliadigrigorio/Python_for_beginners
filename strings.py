dd5a883e3f7c61c7c5c229df8cc644df5bc57823
juliadigrigorio <juliadigrigorio@hotmail.com>
2022-08-21 14:50:12 +0300

    practice

--- strings.py
+++ strings.py
@@ -20,3 +20,82 @@ print('I\'m learning Python. It\'s easy.')
 
 #-----------
 
+print('One \nTwo \nThree')
+
+#-----------
+
+print("\t hey \t there")
+
+#-----------
+
+print("Hi \r there")
+
+#-----------
+
+print("Newline is \\n")
+
+#-----------
+
+print('''
+I'm learning.
+Python's syntax is easy.
+''')
+
+#-----------
+
+'''
+Write a program to output the letters A B C D, each on a separate line.
+'''
+print("""A
+B
+C
+D""")
+
+#-----------
+
+print("Spam" + 'eggs')
+
+#-----------
+
+print("2" + "2")
+
+#-----------
+
+print("spam" * 3)
+print(4 * '2')
+
+#-----------
+
+print("test" * "hi") #ERROR
+
+#-----------
+
+'''
+Create a program to output "hi" 42 times, without any separators, on the same line.
+'''
+print('hi' * 42)
+
+#-----------
+
+'''
+You need to make a program for a leaderboard. 
+The program needs to output the numbers 1 to 9, each on a separate line, followed by a dot:
+1.
+2.
+3.
+'''
+print("""1.
+2.
+3.
+4.
+5.
+6.
+7.
+8.
+9.""")
+
+#-----------
+
+
+
+
