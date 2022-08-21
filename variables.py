e38aa15a1a805e9aa5eb01774b8e7eb5bcd24906
juliadigrigorio <juliadigrigorio@hotmail.com>
2022-08-21 15:04:42 +0300

    variables

--- variables.py
+++ variables.py
@@ -8,3 +8,149 @@ print(z)
 
 #----------
 
+x = 7
+print(x)
+print(x + 3)
+print(x)
+
+#----------
+
+x = 123.456
+print(x)
+x = "This is a string"
+print(x + "!")
+
+#----------
+
+a = 8
+name = "Bob"
+
+print(a * name)
+
+#----------
+
+x = 42
+del x
+
+#----------
+
+x = 2
+del x
+x = 8
+print(x)
+
+#----------
+
+'''
+The provided code stores the value 7 in a variable, and outputs it.
+Change the code to output the value of the variable raised to the power of 3.
+'''
+num = 7
+print(num**3)
+
+#----------
+
+x =input()
+print(x)
+
+#----------
+
+x = input()
+print("You entered: " + x)
+
+#----------
+
+name = input()
+print("Welcome, " + name)
+
+#----------
+
+name = input("Enter your name: ")
+print("Hello, " + name)
+
+#----------
+
+'''
+write the code to take a string input and output it, repeated 10 times. 
+'''
+x = input()
+print(x * 10)
+
+#----------
+
+age = int(input())
+print(age)
+
+#----------
+
+height = float(input())
+print(height)
+
+#----------
+
+age = 42
+print("His age is " + str(age))
+
+#----------
+
+name = input()
+age = input()
+print(name + " is " + age)
+
+#----------
+
+'''
+Write a program to take x and y as input and output the string x, repeated y times.
+'''
+x = input()
+y = int(input())
+print(x * y)
+
+#----------
+
+x = 2
+print(x)
+
+x += 3
+print(x)
+
+#----------
+
+x = 8
+
+x -= 2
+print(x)
+
+x /= 3
+print(x)
+
+x **= 5
+print(x)
+
+#----------
+
+x = "spam"
+print(x)
+
+x += "eggs"
+print(x)
+
+#----------
+
+miles = int(input())
+km = miles * 1.60934
+
+print(km)
+
+#----------
+
+'''
+When you go out to eat, you always tip 20% of the bill amount. 
+Your program needs to take the bill amount as input and output the tip as a float.
+'''
+bill = int(input())
+print((bill) * 20 / 100)
+
+#----------
+
+
