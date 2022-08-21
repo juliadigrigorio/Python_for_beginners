4f2fc77fba1a860d8ef4e0207dc67d5a65269054
juliadigrigorio <juliadigrigorio@hotmail.com>
2022-08-21 14:41:47 +0300

    practice

--- Basic_concepts.py
+++ Basic_concepts.py
@@ -16,5 +16,107 @@ print("Eggs")
 
 #-------------
 
+print(2 + 2)
+print(5 + 4 - 3)
+
+#-------------
+
+print(12 * 64)
+
+#-------------
+
+print(100 / 2)
+
+#-------------
+
+print(2 * (3 + 4))
+
+#-------------
+
+print(-8 * 6)
+
+#-------------
+
+print(123456789 * 12345)
+
+#-------------
+''''
+You need to calculate the points earned by a soccer team. 
+The team won 18 games and ended 7 games as a draw. 
+A win brings 3 points, while a draw brings 1.
+Create a program to calculate and output the total points earned by the team.
+'''
+print(18 * 3 + 7)
+
+#-------------
+
+print(8 + 4 - 2)
+print(10 * 6)
+
+#-------------
+
+print(6 / 2)
+print(8 / 3)
+
+#-------------
+
+print(4.13 * 7)
+print(1.3 + 9.87)
+print(4 + 3.7)
+
+#-------------
+
+'''
+Write a program that will multiply the sum of 5 and 6 by 57.3 and output the result. 
+'''
+print(57.3 * (5 + 6))
+
+#-------------
+
+print(2**5)
+
+#-------------
+
+print(2**3**2)
+
+#-------------
+
+print(9 ** (1 / 2) )
+
+#-------------
+
+print(20 // 6)
+
+#-------------
+
+print(20 % 6)
+
+#-------------
+
+print(1.25 % 0.5)
+print(4.2 // 1.4)
+
+#-------------
+
+'''
+Write code to output 4 raised to the 5th power.
+'''
+print(4**5)
+
+#-------------
+
+'''
+You need to calculate the flight time of an upcoming trip. 
+You are flying from LA to Sydney, covering a distance of 7425 miles, the plane flies at an average speed of 550 miles an hour.
+Calculate and output the total flight time in hours.
+'''
+print(7425/550)
+
+#-------------
+
+
+
+
+
 
 
