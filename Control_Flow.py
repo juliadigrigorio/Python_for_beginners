388885eef8bceb7243db70ccb46b40eedc641c47
juliadigrigorio <juliadigrigorio@hotmail.com>
2022-08-21 14:28:25 +0300

    control flows

---
+++ Control_Flow.py
@@ -0,0 +1,38 @@
+my_boolean = True
+print(my_boolean)
+
+print(2 == 3)
+
+print("hello" == "hello")
+
+#---------
+
+open = True
+closed = False
+
+print(open)
+print(closed)
+
+#---------
+
+x = 7
+
+print(x != 8)
+print(x > 5)
+print(x < 2)
+print(x >= 7)
+print(x <= 7)
+
+#---------
+
+print('a' > 'b')
+
+print("Bob" > "Dave")
+
+#---------
+
+x = (7 > 5)
+print(int(x))
+
+#--------
+
